c81b0dedc0701be0c3cc5f0aaa75bbe59756ee07
Alison Israel <taykarus@gmail.com>
2023-04-06 17:01:10 -0300

    feat: add function to remove duplicated countries

--- app-paises/src/paisesList.js
+++ app-paises/src/paisesList.js
@@ -3,63 +3,72 @@ import { View, Text, TouchableOpacity, ScrollView } from "react-native"
 import { SearchBar } from '@rneui/themed';
 
 export default PaisesList = ({ navigation }) => {
-    const [paises, setPaises] = useState([]);
-    const [paisesFiltrados, setPaisesFiltrados] = useState([]);
-    const [search, setSearch] = useState("");
+  const [paises, setPaises] = useState([]);
+  const [paisesFiltrados, setPaisesFiltrados] = useState([]);
+  const [search, setSearch] = useState("");
 
-    useEffect(() => {
-        fetchPaises();
-      }, []);
-    
-    const updateSearch = (search) => {
-      setSearch(search);
-    };
+  useEffect(() => {
+    fetchPaises();
+  }, []);
 
-    const fetchPaises = async () => {
-        try {
-          const response = await fetch('https://servicodados.ibge.gov.br/api/v1/paises/');
-          const jsonData = await response.json();
-          setPaises(jsonData);
-          setPaisesFiltrados(jsonData);
-        } catch (error) {
-          console.error(error);
+  const updateSearch = (search) => {
+    setSearch(search);
+  };
+
+  const fetchPaises = async () => {
+    try {
+      const response = await fetch('https://servicodados.ibge.gov.br/api/v1/paises/');
+      const paisesData = await response.json();
+      const paises = [];
+      paisesData.forEach((pais) => {
+        const paisNome = pais?.nome?.abreviado
+        const alreadyAdded = paises.findIndex(pais => {
+          return pais.nome.abreviado === paisNome;
+        }) > -1;
+        if (!alreadyAdded) {
+          paises.push(pais);
         }
-      };
+      });
+      setPaises(paises);
+      setPaisesFiltrados(paises);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    useEffect(() => {
-      if (search) {
-        const newPaisesFiltrados = paises.filter(
-          function (pais) {
-            const paisNome = pais?.nome?.abreviado
-                ? pais?.nome?.abreviado?.toUpperCase()
-                : ''.toUpperCase();
-            const pesquisa = search.toUpperCase();
-            return paisNome.indexOf(pesquisa) > -1;
-          }
-        )
-        setPaisesFiltrados(newPaisesFiltrados)
-      }
-    }, [search])
-    return (
-        <View>
-            
-            <Text>Lista de paises</Text>
-            <TouchableOpacity onPress={() => navigation.navigate('HomePage')}>
-                <Text>Home</Text> 
-            </TouchableOpacity>
-            <SearchBar
-              placeholder="Pesquise um país..."
-              onChangeText={updateSearch}
-              value={search}
-            />
-            <ScrollView>
-                {paisesFiltrados.map((pais, index) => (
-                    <TouchableOpacity key={index} onPress={() => navigation.navigate('pais detail', {pais: pais})}>
-                        <Text> {pais.nome.abreviado}</Text>
-                    </TouchableOpacity>
-                    
-                ))}
-            </ScrollView>
-        </View>
-    )
+  useEffect(() => {
+    if (search) {
+      const newPaisesFiltrados = paises.filter(
+        function (pais) {
+          const paisNome = pais?.nome?.abreviado
+            ? pais?.nome?.abreviado?.toUpperCase()
+            : ''.toUpperCase();
+          const pesquisa = search.toUpperCase();
+          return paisNome.indexOf(pesquisa) > -1;
+        }
+      )
+      setPaisesFiltrados(newPaisesFiltrados)
+    }
+  }, [search])
+
+  return (
+    <View>
+      <Text>Lista de paises</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('HomePage')}>
+        <Text>Home</Text>
+      </TouchableOpacity>
+      <SearchBar
+        placeholder="Pesquise um país..."
+        onChangeText={updateSearch}
+        value={search}
+      />
+      <ScrollView>
+        {paisesFiltrados.map((pais, index) => (
+          <TouchableOpacity key={index} onPress={() => navigation.navigate('pais detail', { pais: pais })}>
+            <Text> {pais.nome.abreviado}</Text>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+    </View>
+  )
 }
