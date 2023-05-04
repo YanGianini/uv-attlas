aa10f5280aaa68225873cd289208e5cd263b8993
GeeksMed <silva.germain@gmail.com>
2023-05-04 20:07:42 -0300

    new api

--- app-paises/src/paisesList.js
+++ app-paises/src/paisesList.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { View, Text, TouchableOpacity, ScrollView } from "react-native"
-import { ThemeProvider, SearchBar, ListItem } from '@rneui/themed';
+import { ThemeProvider, SearchBar, ListItem, Avatar } from '@rneui/themed';
 
 export default PaisesList = ({ navigation }) => {
   const [paises, setPaises] = useState([]);
@@ -17,13 +17,13 @@ export default PaisesList = ({ navigation }) => {
 
   const fetchPaises = async () => {
     try {
-      const response = await fetch('https://servicodados.ibge.gov.br/api/v1/paises/');
+      const response = await fetch('https://restcountries.com/v2/all');
       const paisesData = await response.json();
       const paises = [];
       paisesData.forEach((pais) => {
-        const paisNome = pais?.nome?.abreviado
+        const paisNome = pais?.translations?.pt
         const alreadyAdded = paises.findIndex(pais => {
-          return pais.nome.abreviado === paisNome;
+          return pais.translations.pt === paisNome;
         }) > -1;
         if (!alreadyAdded) {
           paises.push(pais);
@@ -40,8 +40,8 @@ export default PaisesList = ({ navigation }) => {
     if (search) {
       const newPaisesFiltrados = paises.filter(
         function (pais) {
-          const paisNome = pais?.nome?.abreviado
-            ? pais?.nome?.abreviado?.toUpperCase()
+          const paisNome = pais?.translations?.pt
+            ? pais?.translations?.pt?.toUpperCase()
             : ''.toUpperCase();
           const pesquisa = search.toUpperCase();
           return paisNome.indexOf(pesquisa) > -1;
@@ -65,9 +65,15 @@ export default PaisesList = ({ navigation }) => {
         <ScrollView>
           {paisesFiltrados.map((pais, index) => (
             <TouchableOpacity key={index} onPress={() => navigation.navigate('País', { pais: pais })}>
-              <ListItem>
+              <ListItem bottomDivider>
+                <Avatar
+                  rounded
+                  source={{ uri: pais.flags.svg }}
+                />
                 <ListItem.Content>
-                  <ListItem.Title>{pais.nome.abreviado}</ListItem.Title>
+                  <ListItem.Title>{pais.translations.pt}</ListItem.Title>
+                  <ListItem.Subtitle>{pais.nativeName}</ListItem.Subtitle>
+                  <ListItem.Subtitle>{pais.flags.svg}</ListItem.Subtitle>
                 </ListItem.Content>
               </ListItem>
             </TouchableOpacity>
