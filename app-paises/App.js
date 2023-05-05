612c65e19bbaeb8a5653eba19239c2f174251fb8
Alison Israel <taykarus@gmail.com>
2023-05-04 21:11:48 -0300

    feat: update home page

--- app-paises/App.js
+++ app-paises/App.js
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import homePage from './src/homePage';
-import paisesList from './src/paisesList';
-import paisDetail from './src/paisDetail';
+import CountryList from './src/components/Country/List';
+import CountryDetail from './src/components/Country/Detail';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,9 +12,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" options={{headerShown: false}} component={homePage} />
-        <Stack.Screen name="Lista de Países" component={paisesList} />
-        <Stack.Screen name="País" component={paisDetail} />
+        <Stack.Screen name="Lista de Países" options={{headerShown: false}} component={CountryList} />
+        <Stack.Screen name="País" component={CountryDetail} />
       </Stack.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
---
+++ app-paises/src/components/Country/Detail/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native"
+
+const styles = StyleSheet.create({
+    countryName: {
+        fontWeight: 'bold',
+        fontSize: 22,
+        textAlign: 'center',
+        backgroundColor: '#0000FF',
+        color: '#FFF',
+    },
+    fontBolder: {
+        fontWeight: 'bold',
+    },
+    fontInfo: {
+        marginLeft: 10,
+        textAlign: 'justify',
+    },
+});
+
+export default styles;
