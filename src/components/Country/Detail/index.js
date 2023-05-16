c864bfa6ca5ccb109975d866c9bb14c739e31ef0
Alison Israel <taykarus@gmail.com>
2023-05-16 17:24:36 -0300

    feat: add tab view in Country detail

--- src/components/Country/Detail/index.js
+++ src/components/Country/Detail/index.js
@@ -1,30 +1,68 @@
-import { Card } from '@rneui/themed';
-import { View, Text, ScrollView } from "react-native"
+import React, { useEffect } from 'react';
+import { Card, Tab, Text, TabView } from '@rneui/themed';
 import styles from './styles'
 
 export default CountryDetail = ({ route, navigation }) => {
 
     const { country } = route.params
+    const [countryData, setCountryData] = React.useState(null);
+    
+    const [index, setIndex] = React.useState(0);
 
-    return country ? (
-        <View>
-            <ScrollView>
-                <Card>
-                    <Text style={styles.countryName}>{country.nome.abreviado}</Text>
-                    <Text style={styles.fontBolder}>Continente:</Text>
-                    <Text style={styles.fontInfo}>{country.localizacao.regiao.nome}</Text>
-                    <Text style={styles.fontBolder}>Capital:</Text>
-                    <Text style={styles.fontInfo}>{country.governo.capital.nome}</Text>
-                    <Text style={styles.fontBolder}>Area:</Text>
-                    <Text style={styles.fontInfo}>{country.area.total} {country.area.unidade['símbolo']}</Text>
-                    <Text style={styles.fontBolder}>Lingua:</Text>
-                    <Text style={styles.fontInfo}>{country.linguas[0].nome}</Text>
-                    <Text style={styles.fontBolder}>Moeda:</Text>
-                    <Text style={styles.fontInfo}>{country['unidades-monetarias'][0].id['ISO-4217-ALPHA']} - {country['unidades-monetarias'][0].nome}</Text>
-                    <Text style={styles.fontBolder}>Historia geral:</Text>
-                    <Text style={styles.fontInfo}>{country.historico}</Text>
-                </Card>
-            </ScrollView>
-        </View>
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('https://servicodados.ibge.gov.br/api/v1/paises/' + country.alpha2Code)
+            const data = await response.json()
+            setCountryData(data[0])
+        }
+        fetchData().catch(console.error);
+    }, [country])
+
+    return countryData ? (
+        <>
+            <Tab
+                value={index}
+                onChange={(e) => setIndex(e)}
+                indicatorStyle={{
+                    backgroundColor: 'white',
+                    height: 3,
+                }}
+                variant="primary"
+            >
+                <Tab.Item
+                    title="Informações"
+                    titleStyle={{ fontSize: 12 }}
+                    icon={{ name: 'book', type: 'ionicon', color: 'white' }}
+                />
+                <Tab.Item
+                    title="Mapa"
+                    titleStyle={{ fontSize: 12 }}
+                    icon={{ name: 'map', type: 'ionicon', color: 'white' }}
+                />
+            </Tab>
+
+            <TabView value={index} onChange={setIndex} animationType="spring">
+                <TabView.Item style={{ backgroundColor: 'gray', width: '100%' }}>
+                    <Card>
+                        <Text style={styles.countryName}>{countryData.nome.abreviado}</Text>
+                        <Text style={styles.fontBolder}>Continente:</Text>
+                        <Text style={styles.fontInfo}>{countryData.localizacao.regiao.nome}</Text>
+                        <Text style={styles.fontBolder}>Capital:</Text>
+                        <Text style={styles.fontInfo}>{countryData.governo.capital.nome}</Text>
+                        <Text style={styles.fontBolder}>Area:</Text>
+                        <Text style={styles.fontInfo}>{countryData.area.total} {countryData.area.unidade['símbolo']}</Text>
+                        <Text style={styles.fontBolder}>Lingua:</Text>
+                        <Text style={styles.fontInfo}>{countryData.linguas[0].nome}</Text>
+                        <Text style={styles.fontBolder}>Moeda:</Text>
+                        <Text style={styles.fontInfo}>{countryData['unidades-monetarias'][0].id['ISO-4217-ALPHA']} - {countryData['unidades-monetarias'][0].nome}</Text>
+                        <Text style={styles.fontBolder}>Historia geral:</Text>
+                        <Text style={styles.fontInfo}>{countryData.historico}</Text>
+                    </Card>
+                </TabView.Item>
+                <TabView.Item style={{ backgroundColor: 'white', width: '100%' }}>
+                    <Text h1>Mapa</Text>
+                </TabView.Item>
+            </TabView>
+        </>
     ) : null
 }
